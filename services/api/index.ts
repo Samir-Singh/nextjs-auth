3bf0279e1ca9fc3f6b514db3e28315d2f6525ca8
Samir Singh <samir@antino.com>
2025-03-05 17:01:54 +0530

    some changes done

--- services/api/index.ts
+++ services/api/index.ts
@@ -1,9 +1,5 @@
 import api from "../axios.interceptor";
 
 export const login = async (email: string, password: string) => {
-  try {
-    return await api.post("/login", { email, password });
-  } catch (error) {
-    throw error;
-  }
+  return await api.post("/login", { email, password });
 };
